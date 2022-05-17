74190f390979c2bbe944bbf279a649323a192ae3
MuzickMan80 <dale@megndale.com>
2022-05-16 23:27:45 -0500

    Initial Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ArenaComponent } from './arena/arena.component';
+import { FrameComponent } from './frame/frame.component';
+import { FilmstripComponent } from './filmstrip/filmstrip.component';
+import { EditorComponent } from './editor/editor.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArenaComponent,
+    FrameComponent,
+    FilmstripComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/filmstrip/filmstrip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filmstrip',
+  templateUrl: './filmstrip.component.html',
+  styleUrls: ['./filmstrip.component.scss']
+})
+export class FilmstripComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
